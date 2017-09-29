3907a15c917112b48e253fd252c00ed4c624eeeb
xiaoyuerlhy <31962700+xiaoyuerlhy@users.noreply.github.com>
2017-09-29 21:37:55 +0800

    Update problem1.6代码.py

--- Chapter1/problem1.6代码.py
+++ Chapter1/problem1.6代码.py
@@ -81,3 +81,34 @@ plt.title("Population,a=100,b=0.01,N(0)=2000") #set title
 plt.ylim(0,2000) #set the range of y axis
 plt.legend()  #make it to show everything
 plt.show()  #activate
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt  #to import matplotlib's package
+
+N=[]       #the population
+t=[]       #time
+a=10       #assign a value to a(the birth)
+b=0        #assign a value to b(the death)
+det_t=0.01     #time step
+N.append(1000) #assign a value to first item of N[]
+t.append(0) #assign a value to first item of t[]
+end_time=2  #total time
+
+for i in range(int(end_time/det_t)):
+    tmp=N[i]+(a*N[i]-b*N[i]*N[i])*det_t  #Euler method
+    N.append(tmp)  #add new value of N to N[]
+    t.append(det_t*(i+1)) #add new value of t to t[]
+    print(t[-1],N[-1]) #print the value of v and t on each stap
+
+plt.figure(figsize=(12,9))  #set the size of corresponding figure
+plt.plot(t,N,label="N(t)",color="blue",linewidth=1) #plot the figure and set label and color and linewidth of the figure
+plt.xlabel("time")   #set the label of x axis
+plt.ylabel("N")  #set the label of y axis
+plt.title("Population,a=10,b=0,N(0)=1000") #set title
+plt.ylim(0,10000000) #set the range of y axis
+plt.legend()  #make it to show everything
+plt.show()  #activate
