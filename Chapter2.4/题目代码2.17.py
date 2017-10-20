6d212b95e5c97a6821568c1b614b14ade4cf83d3
xiaoyuerlhy <31962700+xiaoyuerlhy@users.noreply.github.com>
2017-10-20 18:55:14 +0800

    Update 题目代码2.17.py

--- Chapter2.4/题目代码2.17.py
+++ Chapter2.4/题目代码2.17.py
@@ -5,8 +5,8 @@ import math
 x1=[0]
 y1=[0]
 z1=[0]
-vx1=15
-vy1=15
+vx1=20
+vy1=20
 vz1=0
 dt=0.01
 
@@ -51,22 +51,22 @@ while y2[i]>=0:
     az2=0
     i=i+1
     
-fig = plt.figure(figsize=(14,9))
+fig = plt.figure(figsize=(15,10))
 ax = Axes3D(fig)
 
 
 plt.xlabel("x(m)",fontsize=18)
-plt.ylabel("z(m)",fontsize=18)
+plt.ylabel("z(m)",fontsize=18)    
 ax.set_zlabel("y(m)",fontsize=18)
 plt.title("trajectory of battedball",fontsize=18)
-ax.set_zlim(0,10)
-plt.ylim(-10,0.1)
-plt.xlim(0,40)
+ax.set_zlim(0,15)
+plt.ylim(-20,3)
+plt.xlim(0,100)
 ax.plot(x1, z1, y1,label="trajectory with Magnusforce",color="black",linewidth=2)
 ax.plot(x2, z2, y2,label="trajectory without Magnusforce",color="red",linewidth=2)
 ax.scatter(x1[0],z1[0],y1[0],label="initial position",color="red")
 ax.scatter(x1[-1],z1[-1],y1[-1],label="final position 1",color="black")
 ax.scatter(x2[-1],z2[-1],y2[-1],label="final position 2",color="red")
 plt.legend(loc='upper right',fontsize=12)
-ax.view_init(elev=90, azim=0)   #elev指定z轴的倾斜角度（elevation angle），azim指定x-y平面的方位角（azimuth angle）。
+ax.view_init(elev=60, azim=30)   #elev指定z轴的倾斜角度（elevation angle），azim指定x-y平面的方位角（azimuth angle）。
 plt.show()
